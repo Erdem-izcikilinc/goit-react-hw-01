8d46ee87d6905c8962a39d6e45aa87a1a044b715
Erdem İzcikılınç <izcierdem13@gmail.com>
2025-08-19 14:12:26 +0300

    fix test

--- src/components/FriendList/Friendlist.jsx
+++ src/components/FriendList/Friendlist.jsx
@@ -1,17 +1,19 @@
+// src/components/FriendList/FriendList.jsx
 import React from "react";
-import FriendListItem from "../FriendListItem/FriendListItem";
 import styles from "./FriendList.module.css";
 
 const FriendList = ({ friends }) => {
   return (
     <ul className={styles.list}>
-      {friends.map((friend) => (
-        <li key={friend.id}>
-          <FriendListItem
-            avatar={friend.avatar}
-            name={friend.name}
-            isOnline={friend.isOnline}
-          />
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <li key={id}>
+          <div className={styles.item}>
+            <img src={avatar} alt={name} width="48" className={styles.avatar} />
+            <p className={styles.name}>{name}</p>
+            <p className={isOnline ? styles.online : styles.offline}>
+              {isOnline ? "Online" : "Offline"}
+            </p>
+          </div>
         </li>
       ))}
     </ul>
